a746a1c3e79e2211b6930f92f0af5e5e300186f0
Victor Mesquita Xavier <victor.xavierm@gmail.com>
2020-08-22 12:22:06 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,4 +15,13 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
       
-
+    def comb(self, data):
+        return pd.Series([
+      np.sum([data[nota] for nota in self.columns])/len(self.columns)], index =[f'COMB_{self.name}']
+       )
+          
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data = data.join(data.apply(self.comb, axis=1))
+        return data
