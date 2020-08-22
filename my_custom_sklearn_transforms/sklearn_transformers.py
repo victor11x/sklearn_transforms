83e09d05438890c52ecad09d9ce321feefd1606b
Victor Mesquita Xavier <victor.xavierm@gmail.com>
2020-08-22 12:13:14 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,3 +1,4 @@
+from sklearn.base import BaseEstimator, TransformerMixin
 class DropColumns(BaseEstimator, TransformerMixin):
         def __init__(self, columns):
             self.columns = columns
