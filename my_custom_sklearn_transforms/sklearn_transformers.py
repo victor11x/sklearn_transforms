f3c8c7c838c6c6de4bdf3ce09794184d8ea8cfba
Victor Mesquita Xavier <victor.xavierm@gmail.com>
2020-08-22 02:17:19 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,22 +8,14 @@ class DropColumns(BaseEstimator, TransformerMixin):
             data = X.copy()
             # Retornamos um novo dataframe sem as colunas indesejadas
             return data.drop(labels=self.columns, axis='columns')
-
-class CombMedias(BaseEstimator, TransformerMixin):
-    def __init__(self, columns, name):
-        self.columns = columns
-        self.name = name
-
-    def fit(self, X, y=None):
-        return self
       
-    def comb(self, data):
-        return pd.Series([
+        def comb(self, data):
+            return pd.Series([
         np.sum([data[nota] for nota in self.columns])/len(self.columns)], index =[f'COMB_{self.name}']
         )
           
-    def transform(self, X):
+        def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        data = data.join(data.apply(self.comb, axis=1))
-        return data
+            data = X.copy()
+            data = data.join(data.apply(self.comb, axis=1))
+            return data
